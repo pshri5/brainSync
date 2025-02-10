c0f794de335d9d57dec62fa3984a151ad8621505
Preetam Shri <110012624+pshri5@users.noreply.github.com>
2025-02-10 10:30:44 +0530

    init setup

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from "express"
+import cookieParser from "cookie-parser"
+
+
+const app = express()
+
+app.use(express.urlencoded({extended:true, limit:"16kb"}))
+app.use(express.json({limit:"16kb"}))
+app.use(cookieParser())
+
+
+
+
+export {app}
