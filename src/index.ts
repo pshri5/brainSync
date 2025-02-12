7957f591299ac330114481821873452c122ab5ee
Preetam Shri <110012624+pshri5@users.noreply.github.com>
2025-02-12 10:05:47 +0530

    db connected

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv"
+import { app } from "./app"
+import connectDB from "./db"
+
+
+
+dotenv.config({
+    path: "../.env",
+})
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log(`Server is running on ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{console.log("Mongodb connection error",error)})
