eeeaa5f3d21051be3903e11b893ece5f8b37a0a1
Preetam Shri <110012624+pshri5@users.noreply.github.com>
2025-02-12 10:05:31 +0530

    Add asyncHandler

---
+++ src/utils/asyncHandler.ts
@@ -0,0 +1,11 @@
+//@ts-ignore
+export const asyncHandler = (fn) => async(req,res,next) =>{
+    try {
+        await fn(req,res,next)
+    } catch (error) {
+        res.status(error.code ||  500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
